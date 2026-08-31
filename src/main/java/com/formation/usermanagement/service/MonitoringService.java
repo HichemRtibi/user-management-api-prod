c8a67fa5d3f0ad7c271f70f51582a14d40f65bff
hichem rtibi <hichem.rtibi@gmail.com>
2026-08-31 15:21:37 +0200

    etap e 2

--- src/main/java/com/formation/usermanagement/service/MonitoringService.java
+++ src/main/java/com/formation/usermanagement/service/MonitoringService.java
@@ -16,6 +16,9 @@ public class MonitoringService {
 
     // Notre premier compteur : les inscriptions
     private Counter userRegistrationCounter;
+    private Counter userLoginCounter;        // ← AJOUTER
+    private Counter userLoginFailedCounter;  // ← AJOUTER
+
 
     @PostConstruct
     public void init() {
@@ -28,11 +31,32 @@ public class MonitoringService {
                 .register(meterRegistry);
 
         log.info("✅ Compteur user.registrations.total créé !");
+        userLoginCounter = Counter.builder("user.logins.total")
+                .description("Nombre total de connexions réussies")
+                .tag("service", "user-management")
+                .register(meterRegistry);
+
+        userLoginFailedCounter = Counter.builder("user.logins.failed")
+                .description("Nombre total de connexions échouées")
+                .tag("service", "user-management")
+                .register(meterRegistry);
+
+        log.info("✅ Tous les compteurs sont initialisés !");
     }
 
     // Méthode pour incrémenter le compteur
     public void incrementUserRegistration() {
         userRegistrationCounter.increment();
         log.debug("📈 +1 nouvelle inscription");
+    }
+
+    public void incrementUserLogin() {
+        userLoginCounter.increment();
+        log.debug("📈 +1 connexion réussie");
+    }
+
+    public void incrementUserLoginFailed() {
+        userLoginFailedCounter.increment();
+        log.debug("📈 +1 échec de connexion");
     }
 }
